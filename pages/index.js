50d418b94d81bfa4b42cf06a26633a795165dedf
MischaGithub <mischa.achmat120212@gmail.com>
2020-10-12 13:31:30 +0200

    Connecting to the coindesk api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
 const Index = () => (
@@ -9,5 +10,20 @@ const Index = () => (
     </Layout>
    
 );
+
+// Fetch Req for CoinDesk API
+Index.getInitialProps = async function(){
+
+    // res
+    const res = await fetch
+    (`https://api.coindesk.com/v1/bpi/currentprice.json`);
+    
+    // JSon data
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+}
 
 export default Index;
